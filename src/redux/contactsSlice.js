2bebd032ce2b11ba0a93dd7137bd33d99bea838f
Ivan Zarivnyy <144140704+IvanZarivnyy007@users.noreply.github.com>
2024-08-27 11:00:50 +0200

    hello

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,21 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  inputValue: '',
+  inputValue: [],
   name2: '',
   name3: '',
 };
 
-export const userSlice = createSlice({
+export const contactsSlice = createSlice({
   name: 'name',
   initialState,
   reducers: {
     setName1(state, { payload }) {},
-    setName2(state, { payload }) {},
+    addContact(state, { payload }) {
+      state.payload.push();
+    },
     setName3(state, { payload }) {},
   },
 });
 
-export const { setName1, setName2, setName3 } = userSlice.actions;
+export const { setName1, setName2, setName3 } = contactsSlice.actions;
 
-export default userSlice.reducer;
+export default contactsSlice.reducer;
